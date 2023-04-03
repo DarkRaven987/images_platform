4cae2a4d743ede83dbfbf1f5bcce76aac7b6c1e3
Oleh Tsibuslkyi <darkraven987@gmail.com>
2023-04-03 13:59:28 +0300

    CLIENT: adjusted cypress test code for FormInput component

--- image_platform_client/src/components/FormInput/__tests__/FotmInput.cy.jsx
+++ image_platform_client/src/components/FormInput/__tests__/FotmInput.cy.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import FormInput from '../FormInput';
 
 const testingText = 'test text';
+const containerId = '[data-testid="form-input-container"]';
+const labelId = '[data-testid="form-input-label"]';
+const inputId = '[data-testid="form-input"]';
+const errorId = '[data-testid="form-input-error"]';
 
 describe('FormInput component:', () => {
   it('* renders successfully', () => {
     cy.mount(<FormInput value={testingText} />);
 
-    cy.get('[data-testid="form-input-container"]').should('exist');
-    cy.get('[data-testid="form-input-label"]').should('exist');
-    cy.get('[data-testid="form-input"]')
-      .should('exist')
-      .should('have.prop', 'value', testingText);
-    cy.get('[data-testid="form-input-error"]').should('not.exist');
+    cy.get(containerId).should('exist');
+    cy.get(labelId).should('exist');
+    cy.get(inputId).should('exist').should('have.prop', 'value', testingText);
+    cy.get(errorId).should('not.exist');
   });
 
   it('* accepts and applies attributes', () => {
@@ -29,25 +31,15 @@ describe('FormInput component:', () => {
       />,
     );
 
-    cy.get('[data-testid="form-input-container"]')
+    cy.get(containerId)
       .should('have.class', 'form-input')
       .should('have.class', 'custom-class-name');
 
-    cy.get('[data-testid="form-input-label"]').should(
-      'contains.text',
-      'Test Input',
-    );
+    cy.get(labelId).should('contains.text', 'Test Input');
 
-    cy.get('[data-testid="form-input"]').should(
-      'have.prop',
-      'type',
-      'password',
-    );
+    cy.get(inputId).should('have.prop', 'type', 'password');
 
-    cy.get('[data-testid="form-input-error"]').should(
-      'contains.text',
-      'Field is required',
-    );
+    cy.get(errorId).should('contains.text', 'Field is required');
   });
 
   it('* triggers onChange function', async () => {
@@ -61,8 +53,8 @@ describe('FormInput component:', () => {
       />,
     );
 
-    cy.get('[data-testid="form-input"]').type('New value here');
+    cy.get(inputId).type('New value here');
 
-    cy.get('@click').should('have.been.called');
+    cy.get('@type').should('have.been.called');
   });
 });
