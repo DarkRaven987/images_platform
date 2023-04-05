a495ba7f2a2f227a79d0de263a3fec5fdfc950b4
Oleh Tsibuslkyi <darkraven987@gmail.com>
2023-04-05 15:42:27 +0300

    CLIENT: implemented simple modal to upload image data

--- image_platform_client/src/views/Dashboard/components/AddImageModal/AddImageModal.jsx
+++ image_platform_client/src/views/Dashboard/components/AddImageModal/AddImageModal.jsx
@@ -1,10 +1,38 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import { useState } from 'react';
+import { FileUploader } from 'react-drag-drop-files';
+
 import Modal from '../../../../components/Modal/Modal';
 import './AddImageModal.css';
+import Button from '../../../../components/Button/Button';
 
 const AddImageModal = ({ show, toggle }) => {
+  const [image, setImage] = useState(null);
+  console.log('image', image);
+
+  const fileTypes = ['JPG', 'JPEG', 'PNG', 'SVG'];
+
+  const handleLoadFile = (file) => {
+    const fileData = URL.createObjectURL(file);
+    setImage(fileData);
+  };
+
   return (
     <Modal show={show} toggle={toggle}>
-      Add image goes here
+      <div className="modal-content-container">
+        <FileUploader
+          handleChange={handleLoadFile}
+          name="file"
+          types={fileTypes}
+        />
+        {!!image && <img alt="preview image" src={image} />}
+      </div>
+      <div className="modal-content-actions">
+        <Button className="green-button">Save</Button>
+        <Button className="red-button" onClick={toggle}>
+          Cancel
+        </Button>
+      </div>
     </Modal>
   );
 };
